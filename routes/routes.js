7e6fab0dec4e114b1fcfe5f64acec7d4e6706e5e
GustavoGomesDias <treeelevengg@gmail.com>
2021-04-08 00:23:20 -0300

    Aula 302 - Busca de usuários (findAll e findById

--- routes/routes.js
+++ routes/routes.js
@@ -7,5 +7,7 @@ const UserController = require('../controllers/UserController');
 
 router.get('/', HomeController.index);
 router.post('/user', UserController.create);
+router.get('/user', UserController.index);
+router.get('/user/:id', UserController.findUser);
 
 module.exports = router;
