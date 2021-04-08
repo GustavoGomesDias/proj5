db22bd5db61dc8f83da8f0ef0f9f5f10d1409008
GustavoGomesDias <treeelevengg@gmail.com>
2021-04-07 21:30:37 -0300

    Aula 300 - Validação (inicio, pode haver mais)

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,15 @@
 class UserController{
     async create(req, res){
-        console.log(req.body);
-        res.send("Pegando o corpo da requisição");
+        const { email, name, password } = req.body;
+
+        if(email == undefined || name == undefined || password == undefined){
+            res.status(400);
+            res.json({ err: "Informações inválidas" });
+        }else{
+            res.status(200);
+            res.send("Usuário cadastrado com sucesso!");
+        }
+
     }
 };
 
